f5a15ca3c2bd88d9adb0356a6ee338cfe2b3cd89
byztmain <byztmain2121@gmail.com>
2022-11-24 13:09:58 +0300

    almost ready v- 1.0.0

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,79 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.custom = void 0;
-const Letters_1 = require("./classes/Letters");
-let custom = (limit) => {
-    let NumberCheckBBs = new Letters_1.Letters(limit);
-    return NumberCheckBBs.customizableV15();
+exports.customV15 = exports.customV14 = exports.customV13 = exports.customV12 = exports.customV11 = exports.customV10 = exports.customV9 = exports.customV8 = exports.customV7 = exports.customV6 = exports.customV5 = exports.customV4 = exports.customV3 = exports.customV2 = exports.customV1 = void 0;
+const Main_1 = require("./classes/Main");
+let customV1 = (limit) => {
+    let Value = new Main_1.Letters(limit);
+    return Value.customizableV1();
 };
-exports.custom = custom;
+exports.customV1 = customV1;
+let customV2 = (limit) => {
+    let Value = new Main_1.Letters(limit);
+    return Value.customizableV2();
+};
+exports.customV2 = customV2;
+let customV3 = (limit) => {
+    let Value = new Main_1.Letters(limit);
+    return Value.customizableV3();
+};
+exports.customV3 = customV3;
+let customV4 = (limit) => {
+    let Value = new Main_1.Letters(limit);
+    return Value.customizableV4();
+};
+exports.customV4 = customV4;
+let customV5 = (limit) => {
+    let Value = new Main_1.Letters(limit);
+    return Value.customizableV5();
+};
+exports.customV5 = customV5;
+let customV6 = (limit) => {
+    let Value = new Main_1.Letters(limit);
+    return Value.customizableV6();
+};
+exports.customV6 = customV6;
+let customV7 = (limit) => {
+    let Value = new Main_1.Letters(limit);
+    return Value.customizableV7();
+};
+exports.customV7 = customV7;
+let customV8 = (limit) => {
+    let Value = new Main_1.Letters(limit);
+    return Value.customizableV8();
+};
+exports.customV8 = customV8;
+let customV9 = (limit) => {
+    let Value = new Main_1.Letters(limit);
+    return Value.customizableV9();
+};
+exports.customV9 = customV9;
+let customV10 = (limit) => {
+    let Value = new Main_1.Letters(limit);
+    return Value.customizableV10();
+};
+exports.customV10 = customV10;
+let customV11 = (limit) => {
+    let Value = new Main_1.Letters(limit);
+    return Value.customizableV11();
+};
+exports.customV11 = customV11;
+let customV12 = (limit) => {
+    let Value = new Main_1.Letters(limit);
+    return Value.customizableV12();
+};
+exports.customV12 = customV12;
+let customV13 = (limit) => {
+    let Value = new Main_1.Letters(limit);
+    return Value.customizableV13();
+};
+exports.customV13 = customV13;
+let customV14 = (limit) => {
+    let Value = new Main_1.Letters(limit);
+    return Value.customizableV14();
+};
+exports.customV14 = customV14;
+let customV15 = (limit) => {
+    let Value = new Main_1.Letters(limit);
+    return Value.customizableV15();
+};
+exports.customV15 = customV15;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,65 @@
-import { Letters } from "./classes/Letters";
+import { Letters } from "./classes/Main";
 
 
-export let custom = (limit: number): string => {
-
-    let NumberCheckBBs = new Letters(limit);
-
-    return NumberCheckBBs.customizableV15()
+export let customV1 = (limit: number): string => {
+    let Value = new Letters(limit);
+    return Value.customizableV1()
 }
+export let customV2 = (limit: number): string => {
+    let Value = new Letters(limit);
+    return Value.customizableV2()
+}
+export let customV3 = (limit: number): string => {
+    let Value = new Letters(limit);
+    return Value.customizableV3()
+}
+export let customV4 = (limit: number): string => {
+    let Value = new Letters(limit);
+    return Value.customizableV4()
+}
+export let customV5 = (limit: number): string => {
+    let Value = new Letters(limit);
+    return Value.customizableV5()
+}
+export let customV6 = (limit: number): string => {
+    let Value = new Letters(limit);
+    return Value.customizableV6()
+}
+export let customV7 = (limit: number): string => {
+    let Value = new Letters(limit);
+    return Value.customizableV7()
+}
+export let customV8 = (limit: number): string => {
+    let Value = new Letters(limit);
+    return Value.customizableV8()
+}
+export let customV9 = (limit: number): string => {
+    let Value = new Letters(limit);
+    return Value.customizableV9()
+}
+export let customV10 = (limit: number): string => {
+    let Value = new Letters(limit);
+    return Value.customizableV10()
+}
+export let customV11 = (limit: number): string => {
+    let Value = new Letters(limit);
+    return Value.customizableV11()
+}
+export let customV12 = (limit: number): string => {
+    let Value = new Letters(limit);
+    return Value.customizableV12()
+}
+export let customV13 = (limit: number): string => {
+    let Value = new Letters(limit);
+    return Value.customizableV13()
+}
+export let customV14 = (limit: number): string => {
+    let Value = new Letters(limit);
+    return Value.customizableV14()
+}
+export let customV15 = (limit: number): string => {
+    let Value = new Letters(limit);
+    return Value.customizableV15()
+}
+
 
